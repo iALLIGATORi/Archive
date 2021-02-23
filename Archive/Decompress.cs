5751f4d058e19cd3fa5bce0c140b070e83d92f0a
iALLIGATORi <ALLIGATOR_135790@mail.ru>
2021-02-23 23:02:56 +0300

    Рабочее многопоточное сжатие

---
+++ Archive/Decompress.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace Archive
+{
+    internal class Decompress
+    {
+        public static void Decompressed()
+        {
+            try
+            {
+                using (var sourceStream = Program.fileCompressed.OpenRead())
+                {
+                    using (var targetStream = Program.fileToDecompress.Create())
+                    {
+                        using (var decompressionStream = new GZipStream(sourceStream, CompressionMode.Decompress))
+                        {
+                            decompressionStream.CopyTo(targetStream);
+                            Console.WriteLine(0);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+                Console.WriteLine(1);
+            }
+        }
+    }
+}
